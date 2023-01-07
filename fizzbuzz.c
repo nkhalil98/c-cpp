4730e94e2306d324c6fc7b825e7e02d21974940d
Nabil Khalil <89206827+nkhalil98@users.noreply.github.com>
2023-01-07 03:00:55 -0500

    Update fizzbuzz.c

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -2,11 +2,11 @@
 
 int main() {
   for (int i=1; i <= 100; i++) {
-    if (i%3==0 && i%5==0) {
+    if (i%3 == 0 && i%5 == 0) {
       printf("FizzBuzz\n");
-    } else if (i%3!=0 && i%5==0) {
+    } else if (i%3 != 0 && i%5 == 0) {
         printf("Buzz\n");
-    } else if (i%3==0 && i%5!=0) {
+    } else if (i%3 == 0 && i%5 != 0) {
         printf("Fizz\n");
     } else {
         printf("%d\n", i);
