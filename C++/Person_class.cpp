0d36ea73bfd2bb4a16a0f2ef7aaad919614bba5a
Nabil Khalil <89206827+nkhalil98@users.noreply.github.com>
2023-01-06 21:19:32 -0500

    Add files via upload

---
+++ C++/Person_class.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Person{
+
+    public: //access specifier
+    
+        // begining of constructor
+        Person (string name) {
+            setName(name);
+        }
+        // end of constructor
+
+        void setName (string x) {
+            name = x;
+        }
+
+        string getName () {
+            return name;
+        }
+
+        void speak() {
+            cout << "Hey!" << endl;
+        }
+
+    private:
+        string name;
+};
+
+int main()
+{
+    Person nabil("Nabil");
+    nabil.speak();
+    cout << "My name is " << nabil.getName() << endl;
+    return 0;
+}
